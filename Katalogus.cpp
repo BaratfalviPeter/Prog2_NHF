e707b4661996fcb18d54fa6ccd563fc7057f1519
Barátfalvi Péter <baratfalvipeter@gmail.com>
2026-04-29 21:43:01 +0200

    aprobb javitasok

--- Katalogus.cpp
+++ Katalogus.cpp
@@ -5,10 +5,10 @@ Katalogus::~Katalogus() {}
 
 void Katalogus::atmeretez() {}
 
-Film* Katalogus::getterFilm(int index) const {}
+Film* Katalogus::getterFilm(int index) const {return nullptr;}
 void Katalogus::rendezes() const {}
 void Katalogus::kereses(std::string cim) const {}
-Film* Katalogus::filmLekerdez (std::string cim, int ev) const {}
+Film* Katalogus::filmLekerdez (std::string cim, int ev) const {return nullptr;}
 void Katalogus::listazas() const {}
 void Katalogus::hozzaad(Film* film) {}
 void Katalogus::torles(int index) {}
